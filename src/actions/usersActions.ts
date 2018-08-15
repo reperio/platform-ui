39c7161994e4a981999f843f8539f3f5e2cc16c6
= <reser13@gmail.com>
2018-08-15 11:16:53 -0400

    REP-179 - Removed 403 status code in usersActions

--- src/actions/usersActions.ts
+++ src/actions/usersActions.ts
@@ -14,7 +14,6 @@ function getErrorMessageFromStatusCode(statusCode: number) {
         case 400:
         case 401:
         case 403:
-            return "Invalid username or password";
         default:
             return "An error occurred, please contact your system administrator"}
 }
