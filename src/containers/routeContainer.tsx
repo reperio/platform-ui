a49426e35664493c7b62fac05568404f9f354970
Sean Price <seanprice@sevenhillstechnology.com>
2018-11-13 16:46:30 -0500

    REP-235 Add code to pull otp off of query string

--- src/containers/routeContainer.tsx
+++ src/containers/routeContainer.tsx
@@ -1,6 +1,8 @@
 import React, { ComponentType } from 'react';
 import {connect} from "react-redux";
-import {Redirect, Route, RouteComponentProps} from 'react-router';
+import {Route, RouteComponentProps} from 'react-router';
+import queryString from 'query-string';
+
 import { State } from '../store/initialState';
 import RedirectToLogin from "../components/redirectToLogin";
 
@@ -15,7 +17,12 @@ interface StateProps extends ReturnType<typeof mapStateToProps> {}
 class RouteContainer extends React.Component<RouteComponentProps<any> & StateProps & OwnProps> {
 
     render() {
-        if (!this.props.authSession.isAuthenticated) {
+        const queryParams = queryString.parse(this.props.location.search);
+
+        if (queryParams.otp != null) {
+            console.log("received otp", queryParams.otp);
+            return null;
+        } else if (!this.props.authSession.isAuthenticated) {
             return (<RedirectToLogin />);
         } else {
             return (<Route {...this.props} />);
